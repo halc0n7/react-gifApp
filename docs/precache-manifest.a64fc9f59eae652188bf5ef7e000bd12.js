d331488c12f3f67a2db32a2fb4989a028c3951f5
Manuel Arias <ars.rossess@gmail.com>
2020-10-19 08:28:25 -0500

    docs agregado

---
+++ docs/precache-manifest.a64fc9f59eae652188bf5ef7e000bd12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c452963dda60b0b57abb61ebe519020f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e75cfcd6c5e84209ea",
+    "url": "/static/css/main.b27099fb.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e75cfcd6c5e84209ea",
+    "url": "/static/js/main.e23b37dd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
